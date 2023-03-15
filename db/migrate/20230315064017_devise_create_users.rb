2b8bf6b6ea3ac359cb25866410f36fbd4f00c2d0
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-15 15:54:21 +0900

    usersマイグレーション編集

--- db/migrate/20230315064017_devise_create_users.rb
+++ db/migrate/20230315064017_devise_create_users.rb
@@ -6,7 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :nickname
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.datetime :birth_date
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
