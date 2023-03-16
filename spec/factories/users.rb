ee44776e04d0a2fc6961518015c1a4b2890c166e
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-16 18:35:18 +0900

    FactoryBotで生成するダミーデータ設定

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6, special_characters: false)}
+    password_confirmation {password}
+    nickname {Faker::Restaurant.name}
+    last_name {Faker::Name.last_name}
+    first_name {Faker::Name.first_name}
+    last_name_kana {"タナカ"}
+    first_name_kana {"タロウ"}
+    birth_date {Faker::Date.birthday}
+  end
+end
