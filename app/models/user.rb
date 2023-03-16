8648de2d2d2c4620054cc4e9c228809bad489a5c
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-16 15:49:29 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :password, format: { with: /[a-z\d]{6,}/i, message: 'is invalid. Include both letters and numbers' }
+  
+  validates :nickname, presence: :true
+  validates :last_name, presence: :true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'is invalid. Input full-width characters' }
+  validates :first_name, presence: :true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'is invalid. Input full-width characters' }
+
+  validates :last_name_kana, presence: :true, format: { with: /\A[ァ-ヶ一]+\z/, message: 'is invalid. Input full-width katakana characters' }
+  validates :first_name_kana, presence: :true, format: { with: /\A[ァ-ヶ一]+\z/, message: 'is invalid. Input full-width katakana characters' }
+  validates :birth_date, presence: :true
 end
