729b856d75cc97e41453352405f8598cb7f88fec
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-17 12:15:18 +0900

    Fakerでパスワードを数字・アルファベットの混合で出力する

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     Faker::Config.locale = 'ja'
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6, special_characters: false)}
+    password {Faker::Internet.password(min_length: 6, mix_case: true, special_characters: false)}
     password_confirmation {password}
     nickname {Faker::Restaurant.name}
     last_name {Faker::Name.last_name}
