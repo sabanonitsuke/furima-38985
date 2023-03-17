7bac68bf12d3661b6817815daed3f157ae71c866
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-17 12:28:51 +0900

    Fakerのパスワード出力変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     Faker::Config.locale = 'ja'
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6, mix_case: true, special_characters: false)}
+    password {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
     nickname {Faker::Restaurant.name}
     last_name {Faker::Name.last_name}
