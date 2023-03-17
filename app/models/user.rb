ab4b892102660f00a9a56ed8960a730c90cba79b
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-17 12:00:34 +0900

    パスワードのregexpを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  validates :password, format: { with: /[a-z\d]{6,}/i, message: 'is invalid. Include both letters and numbers' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
   
   validates :nickname, presence: :true
   validates :last_name, presence: :true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'is invalid. Input full-width characters' }
