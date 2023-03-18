e74d3183dd05bcd61bfc9773d672966615630bce
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-18 09:04:44 +0900

    processing_timeクラス編集

--- app/models/processing_time.rb
+++ app/models/processing_time.rb
@@ -1,14 +1,8 @@
 class Processing_time < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '経済' },
-    { id: 3, name: '政治' },
-    { id: 4, name: '地域' },
-    { id: 5, name: '国際' },
-    { id: 6, name: 'IT' },
-    { id: 7, name: 'エンタメ' },
-    { id: 8, name: 'スポーツ' },
-    { id: 9, name: 'グルメ' },
-    { id: 10, name: 'その他' }
+    { id: 2, name: '1~2日で発送' },
+    { id: 3, name: '2~3日で発送' },
+    { id: 4, name: '4~7日で発送' }
   ]
   end
