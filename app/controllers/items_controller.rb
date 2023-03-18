fe543293b1814c68cf8e671711eba721613c9851
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-18 12:51:51 +0900

    ログインしていない場合、ログイン画面に遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,19 @@
 class ItemsController < ApplicationController
+  before_action :redirect_to_index, only: :new
   def index
     
   end
-  
+
   def new
     
   end
+
+  private
+
+  def redirect_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
 end
