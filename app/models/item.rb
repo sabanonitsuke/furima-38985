940841bf687767b0ee69bf1a20e7b6e97809a5fe
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-18 12:27:52 +0900

    items 基本カラムのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,19 @@
 class Item < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_fee_payer_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :processing_time_id, presence: true
+  validates :price, presence: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :prefecture
   belongs_to :Processing_time
   belongs_to :shipping_fee_payer
-  
+
   belongs_to :user
 end
