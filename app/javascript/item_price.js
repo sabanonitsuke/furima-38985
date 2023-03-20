47a496847e237161f1eb164b7d86de787d203f37
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-20 18:12:00 +0900

    手数料・利益が表示される

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,5 +1,13 @@
 function loaded() {
-  console.log("読み込めました")
+  const itemPrice = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const Profit = document.getElementById("profit");
+  itemPrice.addEventListener("keyup", () => {
+    const priceVal = itemPrice.value;
+    const tax = Math.floor(priceVal * 10 / 100)
+    addTaxPrice.innerHTML = Math.floor(tax);
+    Profit.innerHTML = Math.floor(priceVal - tax);
+  });
 };
 
 window.addEventListener('load', loaded);
