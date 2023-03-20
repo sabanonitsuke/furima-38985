3baf0a61ae1b723e0252a33c238853bd54e899c5
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-20 15:39:31 +0900

    createアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,12 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
@@ -19,8 +24,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  def items_params
-    
+  def item_params
+    binding.pry
+    params.require(:item).permit(:title, :description, :category_id, :condition_id, :shipping_fee_payer_id, :prefecture_id, :processing_time_id, :price, :image).merge(:user_id current_user.id)
   end
 
 end
