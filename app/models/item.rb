11bea9a87a8f197f2a19978ab310c7cc99abe88b
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-20 17:04:49 +0900

    特殊バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,13 @@
 class Item < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_fee_payer_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :processing_time_id, presence: true
-  validates :price, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_payer_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :processing_time_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,presence: true, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }, numericality: { greater_than: 299,less_than: 10000000, message: "isout of setting range"}
+  validates :image, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
