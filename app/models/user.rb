4db58c4be9bd13c49db15be7b7c12d8efe474e4b
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-21 14:38:36 +0900

    微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,16 @@ class User < ApplicationRecord
   validates :password,
             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
 
-  validates :nickname, presence: :true
-  validates :last_name, presence: :true,
+  validates :nickname, presence: true
+  validates :last_name, presence: true,
                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
-  validates :first_name, presence: :true,
+  validates :first_name, presence: true,
                          format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
-  validates :last_name_kana, presence: :true,
+  validates :last_name_kana, presence: true,
                              format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
-  validates :first_name_kana, presence: :true,
+  validates :first_name_kana, presence: true,
                               format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
-  validates :birth_date, presence: :true
+  validates :birth_date, presence: true
 
   has_many :items
 end
