9679ba56c070902a597f9b3af3aad5f5d091fbcb
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-22 16:11:46 +0900

    コントローラーにupdateアクション記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :redirect_to_index, only: [:new, :edit]
-  before_action :get_item, only: [:show, :edit]
+  before_action :get_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -23,6 +23,14 @@ class ItemsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
