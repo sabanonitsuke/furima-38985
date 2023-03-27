15d4555194cc2ba0396a93485605fd460a086423
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-27 17:49:57 +0900

    PurchaseDestinationフォームモデル作成

---
+++ app/models/purchase_destination.rb
@@ -0,0 +1,21 @@
+class PurchaseDestination
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+
+
+  with_options presense: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :phone_number
+  end
+  validates :building_name
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
