ca529e027b8cf734389d3c1df479e0aa2d49bd3c
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-28 15:30:17 +0900

    ページ遷移の設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,7 @@
 class PurchasesController < ApplicationController
-  before_action :get_item, only: [:index]
-  before_action :redirect_to_session, only: [:index]
-  before_action :buyer_confirmation, only: [:index]
+  before_action :get_item, only: [:index, :create]
+  before_action :redirect_to_session, only: [:index, :create]
+  before_action :buyer_confirmation, only: [:index, :create]
 
   def index
     @purchase_destination = PurchaseDestination.new
@@ -14,7 +14,6 @@ class PurchasesController < ApplicationController
       @purchase_destination.save
       redirect_to root_path
     else
-      get_item
       render :index
     end
   end
@@ -44,8 +43,8 @@ class PurchasesController < ApplicationController
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],
-      card: order_params[:token],
+      amount: @item.price,
+      card: purchase_params[:token],
       currency: 'jpy'
     )
   end
