88a75713f8a1c7938784df11f88c1738a7e12ce2
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-28 14:55:55 +0900

    決済処理を記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,6 +10,7 @@ class PurchasesController < ApplicationController
   def create
     @purchase_destination = PurchaseDestination.new(purchase_params)
     if @purchase_destination.valid?
+      pay_item
       @purchase_destination.save
       redirect_to root_path
     else
@@ -38,5 +39,14 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase_destination).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token] )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
