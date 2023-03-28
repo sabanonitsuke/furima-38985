5f35222c233dca18138d078ca83b835cda5ddf87
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-28 14:52:03 +0900

    ストロングパラメーターにtoken追加

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -37,6 +37,6 @@ class PurchasesController < ApplicationController
   end
 
   def purchase_params
-    params.require(:purchase_destination).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id] )
+    params.require(:purchase_destination).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token] )
   end
 end
