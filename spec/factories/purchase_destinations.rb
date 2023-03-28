2ac449dda322ee8b06f39eddc5730c54edf8666e
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-28 18:39:29 +0900

    FactoryBotの作成

---
+++ spec/factories/purchase_destinations.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :purchase_destination do
+    token {"tok_abcdefghijk00000000000000000"}
+    postal_code { '123-4567' }
+    prefecture_id { 2 }
+    city { '東京都' }
+    house_number { '1-1' }
+    building_name { '東京ハイツ' }
+    phone_number { '09012345678' }
+  end
+end
