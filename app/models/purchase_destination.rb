decc67c86ba94fd7a93863b8336e1c94e61e0863
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-29 20:33:34 +0900

    商品購入時のエラー文章を日本語化

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -5,14 +5,14 @@ class PurchaseDestination
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'は正しい形式で入力してください(例:123-4567)' }
     validates :city
     validates :house_number
-    validates :phone_number, length: { in: 10..11, message: 'is too short' },
-                             numericality: { only_integer: true, message: 'is invalid. Input only number' }
+    validates :phone_number, length: { in: 10..11, message: 'が短すぎます' },
+                             numericality: { only_integer: true, message: 'は半角数字のみで入力してください' }
     validates :token
   end
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
