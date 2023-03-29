544768d05f2dd08ade27d5c947860d0d2ab1af00
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-29 18:38:43 +0900

    item出品の時のエラー文章日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,15 @@
 class Item < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_fee_payer_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :processing_time_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1, message: "を選択してください" }
+  validates :condition_id, numericality: { other_than: 1, message: "を選択してください" }
+  validates :shipping_fee_payer_id, numericality: { other_than: 1, message: "を選択してください" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
+  validates :processing_time_id, numericality: { other_than: 1, message: "を選択してください" }
   validates :price, presence: true
-  validates :price, numericality: { only_integer: true, message: "isn't integer" }
+  validates :price, numericality: { only_integer: true, message: "は半角数字で入力してください" }
   validates :price,
-            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は指定の範囲で入力してください' }
   validates :image, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
